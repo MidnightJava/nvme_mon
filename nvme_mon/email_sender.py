54938f43f14762e463f5905b82a8cf2a3e45472e
Leone, Mark A [LGS] <mleone607@gmail.com>
2025-12-23 12:18:42 -0500

    Store email settings in .env instead of config.yaml

--- nvme_mon/email_sender.py
+++ nvme_mon/email_sender.py
@@ -3,8 +3,11 @@ from email.mime.text import MIMEText
 import os
 import ssl
 import yaml
+from dotenv import load_dotenv
 
 from nvme_mon.paths import resource_path
+
+load_dotenv(resource_path('.env'))
 
 CONFIG_FILE_NAME = 'config.yaml'
 
@@ -12,18 +15,12 @@ if os.getenv("PRIVATE_CONFIG"): CONFIG_FILE_NAME = "config.private.yaml"
 
 class EmailSender:
 
-    def __init__(self, config_file):
-        if config_file is None:
-            config_file = resource_path(CONFIG_FILE_NAME)
-        with open(config_file, 'r') as f:
-            self.config = yaml.safe_load(f)['email_settings']
-
     def send_email(self, subject, body):
-        recipient_email=self.config['recipient']
-        email_address = self.config['email_address']
-        smtp_server = self.config['smtp_server']
-        smtp_port = self.config['smtp_port']
-        email_password = self.config['email_password']
+        recipient_email=os.env.get('RECIPIENT')
+        email_address =os.env.get('EMAIL_ADDRESS')
+        smtp_server = os.env.get('SMTP_SERVER')
+        smtp_port = os.env.get('SMTP_PORT')
+        email_password = os.env.get('EMAIL_PASSWORD')
 
         msg = MIMEText(body, 'plain')
         msg['From'] = email_address
