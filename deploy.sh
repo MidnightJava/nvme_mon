dabde4f17175d0fe9288fc4a3d1c9c190a26ef49
Leone, Mark A [LGS] <mleone607@gmail.com>
2026-01-16 18:10:43 -0500

    Remove old deploy files

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-sudo systemctl daemon-reload
-sudo systemctl stop nvme-mon || true
-sudo mkdir -p /opt/nvme_mon
-sudo cp -r dist/nvme_mon/* /opt/nvme_mon/
-sudo ln -sf /opt/nvme_mon/nvme_mon /usr/local/bin/nvme_mon
-sudo chown -R root:root /opt/nvme_mon
-sudo chmod -R 755 /opt/nvme_mon
-sudo systemctl start nvme-mon
-sudo systemctl status nvme-mon
--- pybuild.sh
+++
@@ -1,7 +0,0 @@
-python -m pip install -r requirements.txt
-pyinstaller \
-  --onedir \
-  --name nvme_mon \
-  --clean \
-  --noconfirm \
-  main.py
